8f6b9a4120b91cee3a78f2d03ac2bada8ac8c086
Benjamin Vernoux <bvernoux@gmail.com>
2023-05-30 22:33:22 +0200

    mingw-bundledlls v0.2.1 add --verbose mode, clean and optimize whole code

--- mingw-bundledlls.c
+++ mingw-bundledlls.c
@@ -18,28 +18,36 @@
 #include <io.h>
 #include <sys/stat.h>
 #include <sys/utime.h>
-#define PATH_SEPARATOR "\\"
 #else
 #include <unistd.h>
 #include <sys/stat.h>
 #include <utime.h>
-#define PATH_SEPARATOR "/"
 #endif
+
+#define BUFFER_FILE_COPY (2048*1024)
+unsigned char buffer_file_copy[BUFFER_FILE_COPY];
+
+#define PATH_SEPARATOR_LINUX "/"
+#define PATH_SEPARATOR_WINDOWS "\\"
 
 /* External static lib from file Find_DLL_Dependencies.c */
 extern int Find_DLL_Dependencies(const char* filename, char* dependencies[], size_t num_dependencies);
 extern void Free_DLL_Dependencies(char* dependencies[], size_t num_dependencies);
 
 #define APP_NAME "mingw-bundledlls"
-#define VERSION "v0.2.0 29/05/2023 B.VERNOUX"
+#define VERSION "v0.2.1 30/05/2023 B.VERNOUX"
 
 #define BANNER1 APP_NAME " " VERSION "\n"
-#define USAGE "usage: " APP_NAME " <exe_file> [--copy]\n"
+#define USAGE "usage: " APP_NAME " <exe_file> [--copy] [--verbose]\n"
 
 #define ARRAY_LENGTH(array) (sizeof((array))/sizeof((array)[0]))
 
 #define MAX_PATH_LENGTH 1024
 #define MAX_DEPS 10000
+#define MAX_GATHERDEPS 1000
+
+bool verbose = false;
+bool copy_files = false;
 
 const char* DEFAULT_PATH_PREFIXES[] = {
 	"", "/usr/bin", "/usr/i686-w64-mingw32/sys-root/mingw/bin", "/mingw64/bin",
@@ -58,7 +66,8 @@ const char* blacklist_entries[] = {
 	"usp10.dll", "comctl32.dll", "wsock32.dll", "netapi32.dll", "userenv.dll",
 	"avicap32.dll", "avrt.dll", "psapi.dll", "mswsock.dll", "glu32.dll",
 	"bcrypt.dll", "rpcrt4.dll", "hid.dll",
-	"d3d11.dll", "dxgi.dll" // DirectX 3D 11
+	/* DirectX 3D 11 */
+	"d3d11.dll", "dxgi.dll", "dwrite.dll"
 };
 
 typedef struct {
@@ -134,8 +143,20 @@ bool get(HashMap* hashMap, const char* key) {
 
 char* findFullPath(const char* filename, const char* path_prefixes[], int num_prefixes, char* buffer) {
 	for (int i = 0; i < num_prefixes; i++) {
+		/* Detect path separator */
+		char* PATH_SEPARATOR;
+		if(strstr(path_prefixes[i], PATH_SEPARATOR_WINDOWS) != NULL) {
+			PATH_SEPARATOR = PATH_SEPARATOR_WINDOWS;
+		} else {
+			PATH_SEPARATOR = PATH_SEPARATOR_LINUX;
+		}
 		snprintf(buffer, MAX_PATH_LENGTH, "%s%s%s", path_prefixes[i], PATH_SEPARATOR, filename);
-		FILE* file = fopen(buffer, "r");
+		FILE* file = fopen(buffer, "rb");
+		if(verbose) {
+			if(file == NULL) {
+				printf("findFullPath: %s (Not Found)\n", buffer);
+			}
+		}
 		if (file) {
 			fclose(file);
 			return buffer;
@@ -144,18 +165,23 @@ char* findFullPath(const char* filename, const char* path_prefixes[], int num_pr
 	return NULL;
 }
 
-int gatherDeps(const char* path, const char* path_prefixes[], int num_prefixes, HashMap* seen, char* deps[]) {
+int gatherDeps(const char* path, const char* path_prefixes[], int num_prefixes, HashMap* seen, char* deps[], int depth_level) {
 	int num_deps = 0;
-
-	char* dependencies[MAX_DEPS]; // Adjust the array size as per your requirements
+	char* dependencies[MAX_GATHERDEPS]; // Adjust the array size as per your requirements
 	size_t num_dependencies = ARRAY_LENGTH(dependencies);
+
 	int num_dll_deps = Find_DLL_Dependencies(path, dependencies, num_dependencies);
-	if (num_dll_deps < 1) {
+	if(verbose) {
+		printf("dep find(%d-%d deps): %s\n",depth_level, num_dll_deps, path);
+	}
+	if (num_dll_deps < 0) {
 		printf("Error Find_DLL_Dependencies() return %d\n", num_dll_deps);
+		return 0;
+	} else if(num_dll_deps == 0) {
 		return 0;
 	}
 
-	for (size_t dll_deps_idx = 0; dll_deps_idx < num_dll_deps; dll_deps_idx++) {
+	for (int dll_deps_idx = 0; dll_deps_idx < num_dll_deps; dll_deps_idx++) {
 		char* dep = dependencies[dll_deps_idx];
 		char ldep[MAX_PATH_LENGTH + 1];
 		strncpy(ldep, dep, MAX_PATH_LENGTH);
@@ -163,13 +189,21 @@ int gatherDeps(const char* path, const char* path_prefixes[], int num_prefixes, 
 			ldep[i] = tolower(ldep[i]);
 		}
 
-		bool blacklisted = get(&blacklist, ldep);
-		if (blacklisted) {
-			continue;
-		}
-
 		bool already_seen = get(seen, ldep);
 		if (already_seen) {
+			if(verbose) {
+				printf("dep(%d-%d): %s already seen\n",depth_level, dll_deps_idx, ldep);
+			}
+			continue;
+		} else {
+			put(seen, ldep, true);
+		}
+
+		bool blacklisted = get(&blacklist, ldep);
+		if (blacklisted) {
+			if(verbose) {
+				printf("dep(%d-%d): %s blacklisted\n",depth_level, dll_deps_idx, ldep);
+			}
 			continue;
 		}
 
@@ -178,12 +212,20 @@ int gatherDeps(const char* path, const char* path_prefixes[], int num_prefixes, 
 		if (dep_path) {
 			if(num_deps < MAX_DEPS)
 			{
-				put(seen, ldep, true);
+				if(verbose) {
+					printf("dep(%d-%d) found full path: %s\n",depth_level, dll_deps_idx, dep_path);
+				}
 				deps[num_deps++] = strdup(dep_path);
-				int subdeps = gatherDeps(dep_path, path_prefixes, num_prefixes, seen, &deps[num_deps]);
+				depth_level++;
+				int subdeps = gatherDeps(dep_path, path_prefixes, num_prefixes, seen, &deps[num_deps], depth_level);
+				depth_level--;
 				num_deps += subdeps;
 			} else {
-				printf("Error fatal num_deps(%d) >= MAX_DEPS(%d) (increase MAX_DEPS)\n", num_deps, MAX_DEPS);
+				printf("Error fatal num_deps(%d-%d) >= MAX_DEPS(%d) (increase MAX_DEPS)\n",depth_level, num_deps, MAX_DEPS);
+			}
+		} else {
+			if(verbose) {
+				printf("dep(%d-%d): NOT FOUND in full path: %s\n",depth_level, dll_deps_idx, dep);
 			}
 		}
 	}
@@ -212,11 +254,9 @@ void copyFile(const char* source, const char* destination) {
 		return;
 	}
 
-	char buffer[8192];
 	size_t bytesRead;
-
-	while ((bytesRead = fread(buffer, 1, sizeof(buffer), sourceFile)) > 0) {
-		if (fwrite(buffer, 1, bytesRead, destFile) != bytesRead) {
+	while ((bytesRead = fread(buffer_file_copy, 1, sizeof(buffer_file_copy), sourceFile)) > 0) {
+		if (fwrite(buffer_file_copy, 1, bytesRead, destFile) != bytesRead) {
 			printf("Error writing to destination file: %s\n", destination);
 			fclose(sourceFile);
 			fclose(destFile);
@@ -252,28 +292,55 @@ void copyDeps(const char* exe_file, char* deps[], int num_deps) {
 	for (int i = 0; i < num_deps; i++) {
 		char* dep = deps[i];
 		char target[MAX_PATH_LENGTH + 1];
-		snprintf(target, MAX_PATH_LENGTH, "%s%s%s", exe_dir, PATH_SEPARATOR, basename(dep));
-
-		if (strcmp(dep, target) == 0) {
-			printf("Dependency '%s' is already in the target directory, skipping...\n", dep);
+		/* Detect path separator */
+		char* PATH_SEPARATOR;
+		if(strstr(exe_dir, PATH_SEPARATOR_WINDOWS) != NULL) {
+			PATH_SEPARATOR = PATH_SEPARATOR_WINDOWS;
+		} else {
+			PATH_SEPARATOR = PATH_SEPARATOR_LINUX;
 		}
-		else {
-			printf("Copying '%s' to '%s'\n", dep, target);
-			copyFile(dep, target);
+		snprintf(target, MAX_PATH_LENGTH, "%s%s%s", exe_dir, PATH_SEPARATOR, basename(dep));
+		/* Check if target file already exist */
+		FILE* targetFile = fopen(target, "rb");
+		if (targetFile != NULL) {
+			fclose(targetFile);
+			printf("Dependency '%s' target directory already exist, skipping copy...\n", target);
+		} else {
+			if (strcmp(dep, target) == 0) {
+				printf("Dependency '%s' is already in the target directory, skipping copy...\n", dep);
+			} else {
+				printf("Copying '%s' to '%s'\n", dep, target);
+				copyFile(dep, target);
+			}
 		}
 	}
 }
 
 int main(int argc, char* argv[]) {
-
 	const char** path_prefixes;
 	int num_prefixes;
-
 	printf(BANNER1);
 
 	if (argc < 2) {
 		printf(USAGE);
 		return 1;
+	}
+
+	char* exe_file = argv[1];
+	if (argc >= 3 && strcmp(argv[2], "--verbose") == 0) {
+		verbose = true;
+	}
+	if (argc >= 4 && strcmp(argv[3], "--verbose") == 0) {
+		verbose = true;
+	}
+	if (argc >= 3 && strcmp(argv[2], "--copy") == 0) {
+		copy_files = true;
+	}
+	if (argc >= 4 && strcmp(argv[3], "--copy") == 0) {
+		copy_files = true;
+	}
+	if(verbose == true) {
+		printf("verbose\n--copy=%d\nexe_file=\"%s\"\n", copy_files?1:0, exe_file);
 	}
 
 	const char* env_path_prefixes = getenv("MINGW_BUNDLEDLLS_SEARCH_PATH");
@@ -303,31 +370,41 @@ int main(int argc, char* argv[]) {
 		path_prefixes = DEFAULT_PATH_PREFIXES;
 	}
 
-	char* exe_file = argv[1];
+	if(verbose) {
+		printf("path_prefixes(%d):\n", num_prefixes);
+		for(int i = 0; i < num_prefixes; i++) {
+			printf("\"%s\"\n", path_prefixes[i]);
+		}
+		printf("\n");
+	}
 
 	HashMap seen;
 	initHashMap(&seen, MAX_DEPS);
 	initHashMap(&blacklist, MAX_DEPS);
+
+	/* Add the input exe/dll file input as seen to avoid to include it in gatherDeps() */
+	put(&seen, basename(exe_file), true);
 
 	for (int i = 0; i < ARRAY_LENGTH(blacklist_entries); i++) {
 		put(&blacklist, blacklist_entries[i], true);
 	}
 
 	char* deps[MAX_DEPS + 1];
-	int num_deps = gatherDeps(exe_file, path_prefixes, num_prefixes, &seen, deps);
+	int num_deps = gatherDeps(exe_file, path_prefixes, num_prefixes, &seen, deps, 0);
 	if (num_deps > 0) {
 		printf("Dependencies(%d):\n", num_deps);
-		for (int i = 0; i < num_deps; i++) {
-			printf("%s\n", deps[i]);
-		}
-		if (argc >= 3 && strcmp(argv[2], "--copy") == 0) {
+		if (copy_files) { // Copy all dependencies
 			char destination[MAX_PATH_LENGTH + 1];
 			snprintf(destination, MAX_PATH_LENGTH, "%s", dirname(exe_file));
 			copyDeps(destination, deps, num_deps);
+		} else { // Display all dependencies
+			for (int i = 0; i < num_deps; i++) {
+				printf("%s\n", deps[i]);
+			}
 		}
+
 		freeDeps(deps, num_deps);
-	}
-	else {
+	} else {
 		printf("No dependencies found.\n");
 	}
 
@@ -343,3 +420,4 @@ int main(int argc, char* argv[]) {
 
 	return 0;
 }
+
